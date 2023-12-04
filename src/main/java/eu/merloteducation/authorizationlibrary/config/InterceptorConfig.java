eb99003a309647aa95649278ba97fcdb25408ce9
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-12-04 10:51:43 +0100

    add interceptor config

---
+++ src/main/java/eu/merloteducation/authorizationlibrary/config/InterceptorConfig.java
@@ -0,0 +1,19 @@
+package eu.merloteducation.authorizationlibrary.config;
+
+import eu.merloteducation.authorizationlibrary.authorization.ActiveRoleHeaderHandlerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ActiveRoleHeaderHandlerInterceptor activeRoleHeaderHandlerInterceptor;
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(activeRoleHeaderHandlerInterceptor);
+    }
+}
